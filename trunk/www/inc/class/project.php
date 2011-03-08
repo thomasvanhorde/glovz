10505deb5b8fd2bbbe1c80d9f0e60c319d704083
jbcrestot <jbcrestot@1e59827f-18e3-f4c4-15a4-65904377fcf8>
2011-03-08 20:02:31 +0000

    classes basiques - arbo j'ai rien compris

---
+++ trunk/www/inc/class/project.php
@@ -0,0 +1,32 @@
+<?php
+
+class Project {
+
+    public  function __construct() {
+        ;
+    }
+
+    public function defaut() {
+        ;
+    }
+
+    public function create() {
+        ;
+    }
+
+    public function old() {
+        ;
+    }
+
+    public function task() {
+        ;
+    }
+
+    public function milestone() {
+        ;
+    }
+
+    public function member() {
+        ;
+    }
+}
