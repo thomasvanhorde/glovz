7b427cfa0d4ce8ba3acaa29c800bb5573fed6b07
ListenTruth@gmail.com <ListenTruth@gmail.com@1e59827f-18e3-f4c4-15a4-65904377fcf8>
2011-03-11 11:06:22 +0000

    Class simplifié pour accéder au ContentManager

--- www/controller/home/home.controller.php
+++ www/controller/home/home.controller.php
@@ -1,11 +1,7 @@
 <?php
 
 class home_controller {
-    public function __construct() {
-        $this->_contentManager = Base::Load(CLASS_CONTENT_MANAGER);
-    }
+    public function __construct() {}
 
-    public function defaut() {
-        var_dump($this->_contentManager);
-    }
+    public function defaut() {}
 }
