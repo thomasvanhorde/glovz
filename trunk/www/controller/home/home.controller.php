d04b0092239eca01a0088456c2186eca7aade1e8
ListenTruth@gmail.com <ListenTruth@gmail.com@1e59827f-18e3-f4c4-15a4-65904377fcf8>
2011-03-11 10:33:10 +0000

    rename

--- trunk/www/controller/home/home.controller.php
+++ trunk/www/controller/home/home.controller.php
@@ -2,10 +2,10 @@
 
 class home_controller {
     public function __construct() {
-        ;
+        $this->_contentManager = Base::Load(CLASS_CONTENT_MANAGER);
     }
 
     public function defaut() {
-        ;
+        var_dump($this->_contentManager);
     }
 }
