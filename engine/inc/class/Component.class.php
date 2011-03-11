29b668aed9da478cb7bde3ba0e60010b944a9217
ListenTruth@gmail.com <ListenTruth@gmail.com@1e59827f-18e3-f4c4-15a4-65904377fcf8>
2011-03-11 08:53:39 +0000

    Cleaning code

--- engine/inc/class/Component.class.php
+++ engine/inc/class/Component.class.php
@@ -7,13 +7,12 @@
 
 
 
-Class Component {
+class Component {
 	var $_view;
 	var $_bdd;
-	function Component(){
+	public function __construct(){
 		$this->_bdd = Base::Load(CLASS_BDD);
 	}
 }
 
 
-?>
--- engine/inc/class/Email.class.php
+++ engine/inc/class/Email.class.php
@@ -58,4 +58,3 @@ class Email extends PHPMailer{
 
 }
 
-?>
