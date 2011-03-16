68928b988b7c18d952f107e0fd6dd40d924f4d27
ListenTruth@gmail.com <ListenTruth@gmail.com@1e59827f-18e3-f4c4-15a4-65904377fcf8>
2011-03-16 23:04:04 +0000

    delete fucking cache !!

--- www/cache/%%42^426^426435AC%%mail_newMember.tpl.php
+++
@@ -1,4 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2011-03-16 21:36:27
-         compiled from controller/user/view/mail_newMember.tpl */ ?>
-Yeahhh vous êtes désormais inscrit à <a href="<?php echo $this->_tpl_vars['BASE_URL']; ?>
-">Glovz</a> !
