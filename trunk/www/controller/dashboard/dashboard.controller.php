0d93a466e04e8f03d696379c781e983cce68ee3b
ListenTruth@gmail.com <ListenTruth@gmail.com@1e59827f-18e3-f4c4-15a4-65904377fcf8>
2011-03-20 12:58:53 +0000

    projet en cour

--- trunk/www/controller/dashboard/dashboard.controller.php
+++ trunk/www/controller/dashboard/dashboard.controller.php
@@ -12,11 +12,15 @@ class dashboard_controller {
         $this->_dashboardClass = Base::Load(CLASS_DASHBOARD);
         $this->_jalonClass = Base::Load(CLASS_JALON);
         $this->_userClass = Base::Load(CLASS_USER);
+        $this->_userData = $this->_userClass->isConnect();
     }
 
     function defaut(){
         $firstJalon = $this->_jalonClass->myFirst();
         $this->_view->assign('jalon', $firstJalon);
+
+        $myProject = $this->_userClass->getProject($this->_userData['uid']);
+        $this->_view->assign('projects', $myProject);
 
         $this->_view->addBlock('content','defaut.tpl');
         $this->_view->assign('info', $this->_userInfo);
