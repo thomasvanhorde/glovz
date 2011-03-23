228518d3df3f291dd364b0aeee407ffcd47d6412
jbcrestot <jbcrestot@1e59827f-18e3-f4c4-15a4-65904377fcf8>
2011-03-23 21:44:15 +0000

    bug bckgrnd color dans les pop in

--- trunk/www/inc/base.config.php
+++ trunk/www/inc/base.config.php
@@ -17,11 +17,9 @@ Define_once('MONGO_HOST','mongodb://10.70.224.118');
 Define_once('MONGO_BASE','test');
 */
 
-
+/* config chez thomas */
 Define_once('MONGO_HOST','mongodb://192.168.0.23');
 Define_once('MONGO_BASE','test');
-
-
 
 Define_once('MONGO_HOST','localhost');
 Define_once('MONGO_BASE','test');
