fe1825c8f48e0a13d005f6192ce836262a2b01ed
aenyhm <aenyhm@1e59827f-18e3-f4c4-15a4-65904377fcf8>
2011-03-24 03:42:47 +0000

    - Arrangement, nettoyage et commentation de quelques fichiers

--- trunk/www/inc/functions.php
+++ trunk/www/inc/functions.php
@@ -1,51 +1,53 @@
 <?php
 
-/**
- * @param  $name
- * @param  $value
- * @return void
- */
-function Define_once($name, $value){
-    if(!defined($name))
-        define($name, $value);
-}
+	/**
+	 *	Empêche de redéclarer une variable
+	 *
+	 *	@author Thomas Van Horde
+	 *	@version 1.0
+	 */
+	function define_once($name, $value) {
+		if (!defined($name))
+			define($name, $value);
+	}
 
-/**
- * @param  $string
- * @param string $method
- * @return string
- */
-function selEncode($string, $method = 'md5'){
-    switch($method){
-        case 'md5' :
-            return md5($string.GRAIN_SEL);
-        break;
-        case 'sha1' :
-            return sha1($string.GRAIN_SEL);
-        break;
-        case 'base64' :
-            return base64_encode($string.GRAIN_SEL);
-        break;
-    }
-}
+	/**
+	 *	Encodage en md5 ou sha1 avec un grain de sel
+	 *
+	 *	@author Thomas Van Horde
+	 *	@version 1.0
+	 */
+	function selEncode($string, $method = 'md5') {
+		switch ($method) {
+			case 'md5' :
+				return md5($string.GRAIN_SEL);
+			break;
+			case 'sha1' :
+				return sha1($string.GRAIN_SEL);
+			break;
+			case 'base64' :
+				return base64_encode($string.GRAIN_SEL);
+			break;
+		}
+	}
 
-/**
- * @param  $string
- * @param string $method
- * @return 
- */
-function selDecode($string, $method = 'base64'){
-    switch($method){
-        case 'base64' :
-            $tmp = base64_decode($string);
-            $tmp2 = explode(GRAIN_SEL, $tmp);
-            return $tmp2[0];
-        break;
-    }
-}
+	/**
+	 *	Décodage de la chaîne cryptée
+	 *
+	 *	@author Thomas Van Horde
+	 *	@version 1.0
+	 */
+	function selDecode($string, $method = 'base64') {
+		switch($method) {
+			case 'base64' :
+				$tmp = base64_decode($string);
+				$tmp2 = explode(GRAIN_SEL, $tmp);
+				return $tmp2[0];
+			break;
+		}
+	}
 
-
-
-
-
-?>
+/*
+	Fin du fichier : functions.php
+	Chemin du fichier : /www/inc/
+*/
