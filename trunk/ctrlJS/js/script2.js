caaf84d91ab7b4947695f5793193dfbb89a752dd
jbcrestot <jbcrestot@1e59827f-18e3-f4c4-15a4-65904377fcf8>
2011-03-25 11:58:46 +0000

    controle de JS

--- trunk/ctrlJS/js/script2.js
+++ trunk/ctrlJS/js/script2.js
@@ -1,19 +1,54 @@
 function details(id) {
-	$.post('ajax/test.html', function(data) {
-		$('.result').html(data);
-	});
 
 	var html = '';
 	$.ajax({
 		type:"GET",
 		url:"xml/categVideo.xml",
 		dataType:"xml",
-		success: function(xml){
-			html += '<ul>';
-			$(xml).find('categorie').each(function(){
-				html += '<li>
-			});
+		success: function(xml) {
+			$(xml).find('video').each(function(){
+				if ($(this).attr('id') == id) {
+					html += '<h1>Film bientôt dans nos bacs</h1>'
+					html += '<h2>'+
+					$(this).children('nom').text()+
+					'</h2>\n\
+					<img src="'+$(this).children('images').text()+'" alt="'+$(this).children('nom').text()+'" /><br />';
+
+					html += '<h3>Résumé</h3>\n\
+							<p>'+$(this).children('resume').text()+
+							'</p>'+
+							'<h3>tarif :</h3>';
+					$("#corp").html(html);
+				}
+			})
 		}
 	});
-
 }
+
+
+//
+//function erer () {
+//	var html = '';
+//	$.ajax({
+//		type:"GET",
+//		url:"xml/categVideo.xml",
+//		dataType:"xml",
+//		success: function(xml){
+//			$(xml).find('video').each(function(){
+//				if ($(this).children('disponible').text() == 0) {
+//					html += '<h2>'+
+//					$(this).children('nom').text()+
+//					'</h2>\n\
+//					<img src="//'+$(this).children('images').text()+'" alt="'+$(this).children('nom').text()+'" /><br />';
+//
+//					html += '<h3>Résumé</h3>\n\
+//							<p>//'+$(this).children('resume').text()+
+//							'</p>'+
+//							'<h3>tarif :</h3>';
+//					$("#corp").html(html);
+//				}
+//			});
+//		}
+//	});
+//
+//}
